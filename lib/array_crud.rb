2abd4698005cfa3d0b793c0ffd3b43bb315a0dc4
Adam Moran <jeffadammoran@gmail.com>
2019-01-14 18:31:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,9 +27,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array[5]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
   array[-1]
 end
+
+#wow I am really learning arrays!
